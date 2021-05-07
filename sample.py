990e398af9ca55e00852029ad23a3ad8d1b456c3
nately-nate <wheatzend@gmail.com>
2021-05-07 18:17:16 +0900

    commit on preinvestigation

--- sample.py
+++ sample.py
@@ -2,3 +2,5 @@
 print("I don't believe in astrology. I'm saggitarius and they're skeptical.")
 
 print("Abandon all hope ye who enter. 'Inferno'")
+
+print("change sample.py in preinvestigation and merge the branch to the main branch.")
