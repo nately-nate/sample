9b18684d4fc352cd99383574908dd436fbb4f083
nately-nate <wheatzend@gmail.com>
2021-05-07 18:05:04 +0900

    3 quotes

--- sample.py
+++ sample.py
@@ -1,2 +1,6 @@
 
 print("I don't believe in astrology. I'm saggitarius and they're skeptical.")
+
+print("Abandon all hope ye who enter. 'Inferno'")
+
+print("I would like to say I'm person with a particular taste for abstraction.")
